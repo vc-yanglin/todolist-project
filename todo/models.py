b98b81da2cfd545c3d2926358e255a8233fb34b1
vc <victor.csr@gmail.com>
2025-05-09 19:44:54 +0800

    新增資料模型

---
+++ todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.id}-{self.title}-{self.created}"
